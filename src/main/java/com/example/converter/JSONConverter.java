70b470f55b1148356e1ec0d4b17b8374b76b4fb6
Ashish Tiwari <tashish1995@gmail.com>
2022-10-19 00:30:34 +0530

    Updated JSON data.

--- src/main/java/com/example/converter/JSONConverter.java
+++ src/main/java/com/example/converter/JSONConverter.java
@@ -9,40 +9,33 @@ import com.alibaba.fastjson.JSONObject;
 
 @Service
 public class JSONConverter {
-	
+
 	private static final String jsonRequest = "{\r\n"
-			+ "\"record\":\r\n"
+			+ "\"address\":\r\n"
 			+ "    {\r\n"
-			+ "    \"CUACCC\": \" \",\r\n"
-			+ "    \"CUACCR\": \" \",\r\n"
-			+ "    \"CUACPC\": \" \",\r\n"
-			+ "    \"CUADR1\": \"86 BEDFORD ST \",\r\n"
-			+ "    \"CUADR2\": \" \",\r\n"
-			+ "    \"CUADR3\": \" \",\r\n"
-			+ "    \"CUADR4\": \" \",\r\n"
-			+ "    \"CUAGC\": \"TRDAG\",\r\n"
-			+ "    \"CUAICD\": \"1\",\r\n"
-			+ "    \"CUALIA\": \" \",\r\n"
-			+ "    \"RRN_FIELD_DATA\": 92435.0\r\n"
+			+ "    \"addressLine1\": \"24\",\r\n"
+			+ "    \"addressLine2\": \"AMBEDKAR ROAD \",\r\n"
+			+ "    \"addressLine3\": \" \",\r\n"
+			+ "    \"city\": \"GHAZIABAD\",\r\n"
+			+ "    \"state\": \"UP\",\r\n"
+			+ "    \"zipCode\": \"201014\",\r\n"
+			+ "    \"country\": \"INDIA\"\r\n"
 			+ "    },\r\n"
-			+ "\"sv_file_member\": \"ARPCU\",\r\n"
-			+ "\"sv_job_name\": \"RBLP020150\",\r\n"
-			+ "\"sv_job_number\": \"464307\",\r\n"
-			+ "\"sv_journal_seqno\": \"00000000000104898535\",\r\n"
-			+ "\"sv_manip_type\": \"U\"\r\n"
+			+ "\"firstName\": \"Ajay\",\r\n"
+			+ "\"middleName\": \"Kumar\",\r\n"
+			+ "\"lastName\": \"Sharma\",\r\n"
+			+ "\"designation\": \"Senior Application Developer\",\r\n"
+			+ "\"email\": \"example@gmail.com\"\r\n"
 			+ "}";
-	
+
 	public void convertNestedToPlainJSON() {
 		JSONObject jsonObject = JSON.parseObject(jsonRequest);
-		Map<String, Object> map = (Map<String, Object>) jsonObject.get("record");
-		jsonObject.remove("record");
+		Map<String, Object> map = (Map<String, Object>) jsonObject.get("address");
+		jsonObject.remove("address");
 		for (Map.Entry<String, Object> entry : map.entrySet()) {
 			jsonObject.put(entry.getKey(), entry.getValue());
 		}
-
-		jsonObject.remove("record"); 
 		System.out.println(jsonObject);
-
 	}
 
 }
